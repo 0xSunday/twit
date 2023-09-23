f5fa46c41d7571fd8a3b8701abe6fab175d51a70
Sunil Reddy <sunday7637@gmail.com>
2023-09-23 19:13:17 +0530

    fixing bug

--- app/api/current/route.ts
+++ app/api/current/route.ts
@@ -1,22 +1,37 @@
-// import { NextApiRequest, NextApiResponse } from "next";
-import { NextRequest, NextResponse } from "next/server";
 import serverAuth from "@/libs/serverAuth";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function GET(req: Request) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method !== "GET") {
-    return new Response("error");
+    return res.status(405).end();
   }
 
   try {
-    const { currentUser } = await serverAuth(req, Response);
-
-    return new Response(JSON.stringify({ currentUser }), {
-      status: 200,
-    });
-  } catch (error) {
-    console.log(error);
-    return new Response("error", {
-      status: 405,
-    });
+    const { currentUser } = await serverAuth(req, res);
+    return res.status(200).json(currentUser);
+  } catch (error: any) {
+    return res.status(401).end();
   }
 }
+
+// import { NextApiRequest, NextApiResponse } from 'next';
+
+// import serverAuth from '@/libs/serverAuth';
+
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//   if (req.method !== 'GET') {
+//     return res.status(405).end();
+//   }
+
+//   try {
+//     const { currentUser } = await serverAuth(req, res);
+
+//     return res.status(200).json(currentUser);
+//   } catch (error) {
+//     console.error(error); // Changed console.log to console.error for error messages
+//     return res.status(500).json({ error: 'Internal Server Error' }); // Changed status code to 500 for internal server errors
+//   }
+// }
--- libs/serverAuth.ts
+++ libs/serverAuth.ts
@@ -1,7 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 import prisma from "@/libs/prismadb";
-// import { authOptions } from '@/pages/api/auth/[...nextauth]authorize';
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
