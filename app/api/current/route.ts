96762321035beb699b660f62f7dc13502b1ad6d1
Sunil Reddy <sunday7637@gmail.com>
2023-09-24 20:09:15 +0530

    adding auth

--- app/api/current/route.ts
+++ app/api/current/route.ts
@@ -6,32 +6,13 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method !== "GET") {
-    return res.status(405).end();
+    return res.status(405).send("Method not allowed");
   }
 
   try {
     const { currentUser } = await serverAuth(req, res);
-    return res.status(200).json(currentUser);
+    return res.status(200).json({ currentUser });
   } catch (error: any) {
-    return res.status(401).end();
+    return res.status(401).send("Unauthorized");
   }
 }
-
-// import { NextApiRequest, NextApiResponse } from 'next';
-
-// import serverAuth from '@/libs/serverAuth';
-
-// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-//   if (req.method !== 'GET') {
-//     return res.status(405).end();
-//   }
-
-//   try {
-//     const { currentUser } = await serverAuth(req, res);
-
-//     return res.status(200).json(currentUser);
-//   } catch (error) {
-//     console.error(error); // Changed console.log to console.error for error messages
-//     return res.status(500).json({ error: 'Internal Server Error' }); // Changed status code to 500 for internal server errors
-//   }
-// }
